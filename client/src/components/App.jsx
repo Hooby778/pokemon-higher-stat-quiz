e2dce5b67b0acbcc909d422e67a6c86076f2d305
Tanner <tanjac00@yahoo.com>
2024-07-11 10:28:20 -0600

    Finished Product

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -33,64 +33,78 @@ function App () {
   var randChoice2 = getRandomInt(1024);
   if (pokemon1 && pokemon2 && !endScreen) {
     return (
-      <div>
-        <p>High Score: {highScore}</p>
-        <p>Current Score: {score}</p>
-        <h2>Which Pokemon has the higher {randStat}?</h2>
-        <img src={pokemon1.Photo}></img>
-        <button onClick={() => {
-          if (pokemon1[randStat] >= pokemon2[randStat]) {
-            setScore(score + 1);
-          } else {
-            if (score > highScore) {
-              axios.patch('/pokemon', {"HP": score})
-              setHighScore(score);
-            }
-            setEndScreen(true);
-          }
-        }}>{pokemon1.Name}</button>
-        <img src={pokemon2.Photo}></img>
-        <button onClick={() => {
-          if (pokemon2[randStat] >= pokemon1[randStat]) {
-            setScore(score + 1);
-          } else {
-            if (score > highScore) {
-              axios.patch('/pokemon', {"HP": score})
-              setHighScore(score);
-            }
-            setEndScreen(true);
-          }
-        }}>{pokemon2.Name}</button>
+      <div className="main">
+        <p className="score">High Score: {highScore}</p>
+        <p className="score">Current Score: {score}</p>
+        <h2 className="question">Which Pokemon has the higher {randStat}?</h2>
+        <div className="mons">
+          <div className="mon">
+            <img className="monImg" src={pokemon1.Photo}></img>
+            <button className="monButton" onClick={() => {
+              if (pokemon1[randStat] >= pokemon2[randStat]) {
+                setScore(score + 1);
+              } else {
+                if (score > highScore) {
+                  axios.patch('/pokemon', {"HP": score})
+                  setHighScore(score);
+                }
+                setEndScreen(true);
+              }
+            }}>{pokemon1.Name}</button>
+          </div>
+          <div className="mon">
+            <img className="monImg" src={pokemon2.Photo}></img>
+            <button className="monButton" onClick={() => {
+              if (pokemon2[randStat] >= pokemon1[randStat]) {
+                setScore(score + 1);
+              } else {
+                if (score > highScore) {
+                  axios.patch('/pokemon', {"HP": score})
+                  setHighScore(score);
+                }
+                setEndScreen(true);
+              }
+            }}>{pokemon2.Name}</button>
+          </div>
+        </div>
       </div>
     )
   } else if (pokemon1 && pokemon2 && endScreen) {
     return (
-      <div>
-        <h2>Nice Try!</h2>
-        <p>High Score: {highScore}</p>
-        <p>Your Score: {score}</p>
-        <button onClick={() => {
-          setEndScreen(false);
-          setScore(0);
-        }}>Play Again</button>
-        <img src={pokemon1.Photo}></img>
-        <ul>
-          <li>HP: {pokemon1.HP}</li>
-          <li>Attack: {pokemon1.Attack}</li>
-          <li>Defense: {pokemon1.Defense}</li>
-          <li>Special Attack: {pokemon1['Special Attack']}</li>
-          <li>Special Defense: {pokemon1['Special Defense']}</li>
-          <li>Speed: {pokemon1.Speed}</li>
-        </ul>
-        <img src={pokemon2.Photo}></img>
-        <ul>
-          <li>HP: {pokemon2.HP}</li>
-          <li>Attack: {pokemon2.Attack}</li>
-          <li>Defense: {pokemon2.Defense}</li>
-          <li>Special Attack: {pokemon2['Special Attack']}</li>
-          <li>Special Defense: {pokemon2['Special Defense']}</li>
-          <li>Speed: {pokemon2.Speed}</li>
-        </ul>
+      <div className="main">
+        <h2 className="question">Nice Try!</h2>
+        <p className="score">High Score: {highScore}</p>
+        <p className="score">Your Score: {score}</p>
+        <div className="playButton">
+          <button onClick={() => {
+            setEndScreen(false);
+            setScore(0);
+          }}>Play Again</button>
+        </div>
+        <div className="mons">
+          <div className="pgMon">
+            <img src={pokemon1.Photo}></img>
+            <ul className="statList">
+              <li>HP: {pokemon1.HP}</li>
+              <li>Attack: {pokemon1.Attack}</li>
+              <li>Defense: {pokemon1.Defense}</li>
+              <li>Special Attack: {pokemon1['Special Attack']}</li>
+              <li>Special Defense: {pokemon1['Special Defense']}</li>
+              <li>Speed: {pokemon1.Speed}</li>
+            </ul>
+          </div>
+          <div className="pgMon">
+          <img src={pokemon2.Photo}></img>
+            <ul className="statList">
+              <li>HP: {pokemon2.HP}</li>
+              <li>Attack: {pokemon2.Attack}</li>
+              <li>Defense: {pokemon2.Defense}</li>
+              <li>Special Attack: {pokemon2['Special Attack']}</li>
+              <li>Special Defense: {pokemon2['Special Defense']}</li>
+              <li>Speed: {pokemon2.Speed}</li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   } else {
